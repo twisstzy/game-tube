59d05b1bfa4a8637416c4987d1ce22303c0cc346
Bahram <bahram1m@outlook.com>
2024-10-26 06:53:17 +0000

    remove the api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "55bd4bf7b4af4433b1e3031bbe703cc7",
+    key: "1283192731231",
   },
 });
